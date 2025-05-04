fafa3d3635bf637cd2038183690ce392c4c3215d
adrianit248 <adrianit248@gmail.com>
2025-05-04 19:40:15 -0400

    added bookinfo page and made prices and ratings dynamic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import LibraryLogo from '../assets/Library.svg'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
     function openMenu() {
@@ -14,21 +15,21 @@ const Nav = () => {
     return (
         <nav>
             <div className="nav__container">
-                <a href="/">
+                <Link to="/">
                     <img src={LibraryLogo} alt="" className='logo' />
-                </a>
+                </Link>
                 
                 <ul className='nav__links'>
                     <li className='nav__list'>
-                        <a href="/" className='nav__link'>
+                        <Link to="/" className='nav__link'>
                             Home
-                        </a>
+                        </Link>
                     </li>
 
                     <li className='nav__list'>
-                        <a href="/" className='nav__link'>
+                        <Link to="/books" className='nav__link'>
                             Books
-                        </a>
+                        </Link>
                     </li>
 
                     <button className="btn__menu" onClick={openMenu}>
@@ -36,9 +37,9 @@ const Nav = () => {
                     </button>
 
                     <li className="nav__icon">
-                        <a href="/cart" className='nav__link'>
+                        <Link to="/cart" className='nav__link'>
                             <FontAwesomeIcon icon="shopping-cart" />
-                        </a>
+                        </Link>
                         <span className="cart__length">3</span>
                     </li>
                 </ul>
@@ -50,19 +51,19 @@ const Nav = () => {
 
                     <ul className="menu__links">
                         <li className="menu__list">
-                            <a href="/" className="menu__link">
+                            <Link to="/" className="menu__link">
                                 Home
-                            </a>
+                            </Link>
                         </li>
                         <li className="menu__list">
-                            <a href="/books" className="menu__link">
+                            <Link to="/books" className="menu__link">
                                 Books
-                            </a>
+                            </Link>
                         </li>
                         <li className="menu__list">
-                            <a href="/cart" className="menu__link">
+                            <Link to="/cart" className="menu__link">
                                 Cart
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
